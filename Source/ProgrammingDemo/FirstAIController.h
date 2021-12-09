2d5fe7d375b289304f17336520b8448068ede3a0
Britt <brittboudreaux@yahoo.com>
2021-12-09 10:02:04 -0600

    Added NPC controller for ai and a drain manager to adjust speed of ai drains

---
+++ Source/ProgrammingDemo/FirstAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "FirstAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMINGDEMO_API AFirstAIController : public AAIController
+{
+	GENERATED_BODY()
+
+};
